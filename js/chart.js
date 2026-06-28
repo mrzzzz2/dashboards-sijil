112ed270775a83831612f7d8327e8bd0e720e13e
mrzzzz2 <willsonl.zzz2@gmail.com>
2026-06-28 16:47:30 +0800

    Refactor drawKelasChart to include click event

--- js/chart.js
+++ js/chart.js
@@ -1,4 +1,34 @@
-function drawKelasChart(labels,data){
+function drawKelasChart(labels, values, rawData) {
+
+  if (window.kelasChart) window.kelasChart.destroy();
+
+  window.kelasChart = new Chart(
+    document.getElementById("kelasChart"),
+    {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [{
+          label: "Kelas",
+          data: values
+        }]
+      },
+      options: {
+        onClick: function(evt, elements) {
+
+          if (!elements.length) return;
+
+          const index = elements[0].index;
+          const selectedKelas = labels[index];
+
+          const filtered = filterByKelas(rawData, selectedKelas);
+
+          renderList(filtered);
+        }
+      }
+    }
+  );
+}
 
 new Chart(
 
