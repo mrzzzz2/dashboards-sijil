44df041b554a08ce3fd401ccc7446fc55f150fcd
mrzzzz2 <willsonl.zzz2@gmail.com>
2026-06-28 14:02:39 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,16 +2,10 @@ async function initDashboard() {
 
   const data = await fetchData();
 
+  console.log("RAW DATA:", data);
+
   document.getElementById("jumlahRekod").innerHTML = data.length;
 
-  // ❗ Step 5 就放在这里 ↓↓↓
-
-  const kelasCount = countByField(data, "kelas");
-
-  const labels = Object.keys(kelasCount);
-  const values = Object.values(kelasCount);
-
-  drawKelasChart(labels, values);
 }
 
 initDashboard();
