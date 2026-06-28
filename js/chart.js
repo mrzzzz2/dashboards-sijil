d7ef4f2fb66f2ca0483cd73ad46ba4aa9425b577
mrzzzz2 <willsonl.zzz2@gmail.com>
2026-06-28 14:32:31 +0800

    Refactor drawKelasChart function for clarity

--- js/chart.js
+++ js/chart.js
@@ -25,3 +25,45 @@ data:data
 });
 
 }
+
+let jenisChart;
+
+function drawJenisChart(labels, data) {
+
+  if (jenisChart) jenisChart.destroy();
+
+  jenisChart = new Chart(
+    document.getElementById("jenisChart"),
+    {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [{
+          label: "Jenis Rekod",
+          data: data
+        }]
+      }
+    }
+  );
+}
+
+let peringkatChart;
+
+function drawPeringkatChart(labels, data) {
+
+  if (peringkatChart) peringkatChart.destroy();
+
+  peringkatChart = new Chart(
+    document.getElementById("peringkatChart"),
+    {
+      type: "bar",
+      data: {
+        labels: labels,
+        datasets: [{
+          label: "Peringkat",
+          data: data
+        }]
+      }
+    }
+  );
+}
