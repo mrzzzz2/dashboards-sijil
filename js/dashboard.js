ec0b11519cdd21a62484c2a164b30cf13bc5c761
mrzzzz2 <willsonl.zzz2@gmail.com>
2026-06-28 16:15:14 +0800

    Refactor dashboard.js to include data rendering functions
    Added renderList and filterByField functions to handle data display and filtering.

--- js/dashboard.js
+++ js/dashboard.js
@@ -56,3 +56,43 @@ function countByField(data, field) {
 
   return result;
 }
+
+function renderList(data) {
+
+  const container = document.getElementById("listContent");
+
+  if (!data || data.length === 0) {
+    container.innerHTML = "Tiada data";
+    return;
+  }
+
+  let html = "<table border='1' cellpadding='5' cellspacing='0'>";
+
+  html += `
+    <tr>
+      <th>Nama</th>
+      <th>Kelas</th>
+      <th>Jenis</th>
+      <th>Peringkat</th>
+    </tr>
+  `;
+
+  data.forEach(item => {
+    html += `
+      <tr>
+        <td>${item.nama_pelajar}</td>
+        <td>${item.kelas}</td>
+        <td>${item.jenis_rekod}</td>
+        <td>${item.peringkat}</td>
+      </tr>
+    `;
+  });
+
+  html += "</table>";
+
+  container.innerHTML = html;
+}
+
+function filterByField(data, field, value) {
+  return data.filter(item => item[field] === value);
+}
